2fe51e85293fd8ebaa65ea3739788f1a6fdd1b15
santiagosayshey <chaucted@gmail.com>
2022-07-26 22:56:57 +0930

    tidies up style for cardLoop function

--- cardLoop.cpp
+++ cardLoop.cpp
@@ -1,4 +1,5 @@
 // this function calculates the score for a hand
+
 #include <iostream>
 
 using namespace std;
@@ -7,30 +8,27 @@ extern int scorefinder(int i, int* hand, int score);
 
 int cardLoop(int* hand) {
 
-
-    int score = 0;
-    int count = 0;
-    int i;
+    int score = 0, count = 0, i;
     bool add;
 
     cout << "You Drew: " << endl;
     for (i=0 ; i<2 ; i++) {
         score = scorefinder(i, hand, score);
     }
-    cout << endl;
-    cout << "Score: " << score << endl;
-
-
-    cout << "Would you like to hit? ";
-    cin >> add;
     count=2;
+    cout << endl << "Score: " << score << endl;
+
+    cout << "Would you like to hit? " << endl;
+    cin >> add;
+    
     while (add == true && score <= 21) {
+
         cout << "You Drew: " << endl;
         score = scorefinder(i, hand, score);
         count += 1;
 
-        cout << endl;
-        cout << "Score: " << score << endl;
+        cout << endl << "Score: " << score << endl;
+
         if (score >= 21) {
             break;
         }
