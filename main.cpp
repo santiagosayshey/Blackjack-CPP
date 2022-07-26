b7aa239ec626067183e28b66c3ce9a7d9aad82d3
santiagosayshey <chaucted@gmail.com>
2022-07-26 21:19:19 +0930

    main updated as per previous commit

--- main.cpp
+++ main.cpp
@@ -4,34 +4,18 @@
 using namespace std;
 
 extern int *shuffleDeck();
+extern int *handDraw(int* deck, int choice);
 
 int main() {
 
     int *deck = shuffleDeck();
 
-    for (int i=0 ; i < 52; i++) {
-        cout << deck[i] << endl;
-    }
-    cout << "\n \n \n" << endl;
+    int choice = 1; // player
+    int *playerHand = handDraw(deck, choice);
 
-    static int playerHand[26], dealerHand[26];
-
-    for (int i=0 ; i < 52; i++) {
-
-        // assign to dealer / player in advance
-        if (i%2 == 0) {
-            playerHand[i/2]=deck[i]; 
-        }
-        else {
-            dealerHand[((i/2))]=deck[i];
-        }
-        
-    }
-
-    for (int i=0 ; i < 26; i++) {
-        cout << playerHand[i] << endl;
-        cout << dealerHand[i] << endl;
-    }
+    choice = 2; // dealer
+    int *dealerHand = handDraw(deck, choice);
 
     return 0;
+
 }
