36e41771bc8325b9a7c56fc46ae5c2cc84474034
santiagosayshey <chaucted@gmail.com>
2022-07-26 20:40:27 +0930

    main now adds cards to player and dealers hand

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -8,8 +9,28 @@ int main() {
 
     int *deck = shuffleDeck();
 
-    for (int i = 0; i < 52; i++) {
+    for (int i=0 ; i < 52; i++) {
         cout << deck[i] << endl;
+    }
+    cout << "\n \n \n" << endl;
+
+    static int playerHand[26], dealerHand[26];
+
+    for (int i=0 ; i < 52; i++) {
+
+        // assign to dealer / player in advance
+        if (i%2 == 0) {
+            playerHand[i/2]=deck[i]; 
+        }
+        else {
+            dealerHand[((i/2))]=deck[i];
+        }
+        
+    }
+
+    for (int i=0 ; i < 26; i++) {
+        cout << playerHand[i] << endl;
+        cout << dealerHand[i] << endl;
     }
 
     return 0;
