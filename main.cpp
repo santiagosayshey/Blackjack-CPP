05198db9d736dffe1a3a481b1c7e0d179c860d01
santiagosayshey <chaucted@gmail.com>
2022-07-27 01:25:06 +0930

    added win status and choice to keep playing. v1.0

--- main.cpp
+++ main.cpp
@@ -8,28 +8,42 @@ extern int *handDraw(int* deck, int choice);
 extern void boardState(int *deck);
 extern int playerLoop(int* hand); 
 extern int dealerLoop(int* hand);
+extern void winState(int playerScore, int dealerScore);
 
 int main() {
 
+
+
     int choice;
-    int *hand;
+    int* hand;
+    bool on = true;
 
-    // shuffle the deck
-    int *deck = shuffleDeck();
+    while (on == true) {
 
-    // dealer inital output
-    boardState(deck);
+        // shuffle the deck
+        int *deck = shuffleDeck();
 
-    // player component
-    choice = 1; 
-    hand = handDraw(deck, choice);
-    int playerCount = playerLoop(hand);
+        // dealer inital output
+        boardState(deck);
+
+        // player loop
+        choice = 1; 
+        hand = handDraw(deck, choice);
+        int playerScore = playerLoop(hand);
 
 
-    // dealer component
-    choice = 2;
-    hand = handDraw(deck, choice);
-    int dealerCount = dealerLoop(hand);
+        // dealer loop
+        choice = 2;
+        hand = handDraw(deck, choice);
+        int dealerScore = dealerLoop(hand);
+
+        // win state
+        winState(playerScore, dealerScore);
+
+        cout << "Do you want to keep playing? ";
+        cin>> on;
+
+    }
 
     return 0;
 
---
+++ winState.cpp
@@ -0,0 +1,27 @@
+// this function determin who wins 
+
+#include <iostream>
+
+using namespace std;
+
+void winState(int playerScore, int dealerScore) {
+
+    if (playerScore > 21) { 
+        cout << "Bust! The Dealer Wins!" << endl;
+    }
+    else if (dealerScore > 21) {
+        cout << "Player Wins!" << endl;
+    }
+    else if (playerScore > dealerScore) {
+        cout << "Player Wins!" << endl;
+        if (playerScore == 21) {
+            cout << "Blackjack!" << endl;
+        }
+    }
+    else if (dealerScore == playerScore) {
+        cout << "It's a tie!" << endl;
+    }
+    else {
+        cout << "The Dealer Wins!" << endl;
+    }
+}
